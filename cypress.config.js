b11ce4c7ac7baf03ea3faf67f4776380e0562910
Arthur <arthuras1@hotmail.com>
2026-04-28 14:01:09 -0300

    fix: update viewport to resolve element overlap in headless mode

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,8 @@ require('dotenv').config()
 module.exports = defineConfig({
   e2e: {
     defaultCommandTimeout: 10000,
-    
+    viewportWidth: 1920,
+    viewportHeight: 1080,
     setupNodeEvents(on, config) {
       config.env.USERNAME = process.env.CYPRESS_USERNAME
       config.env.PASSWORD = process.env.CYPRESS_PASSWORD
