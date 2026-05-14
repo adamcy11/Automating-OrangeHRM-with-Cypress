2691d2f70729377753a1b83c0116483310e1415c
Arthur <arthuras1@hotmail.com>
2026-05-14 14:00:41 -0300

    feat: add Mochawesome HTML reports and GitHub Pages deployment

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,14 @@ const { defineConfig } = require("cypress");
 require('dotenv').config()
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'OrangeHRM Test Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     defaultCommandTimeout: 10000,
     viewportWidth: 1920,
@@ -11,11 +19,11 @@ module.exports = defineConfig({
       openMode: 0
     },
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on)
       config.env.USERNAME = process.env.CYPRESS_USERNAME
       config.env.PASSWORD = process.env.CYPRESS_PASSWORD
       return config
     },
-
     baseUrl: 'https://opensource-demo.orangehrmlive.com/web/index.php/'
   },
 });
